0a73e6ef05645f5b3f09da2081d98f561ecb4271
milaotou001 <1402204668@qq.com>
2026-06-05 12:30:16 +0800

    fix: force-dynamic page rendering and reload nginx after deploy to clear cache

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { ProposalHelperApp } from "@/components/ProposalHelperApp";
 
+export const dynamic = "force-dynamic";
+
 export default function ProposalHelperPage() {
   return <ProposalHelperApp />;
 }
